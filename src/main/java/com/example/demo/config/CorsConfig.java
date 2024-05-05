8e0463b5df8342871938237668a763efd0bc288b
thien <tranquocthien@aol.com>
2024-05-05 23:09:57 +0700

    update API

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -1,15 +1,17 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080") // Các domain được phép truy cập API của bạn
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP được phép
+                .allowedHeaders("*"); // Các header được phép (có thể chỉ định cụ thể)
     }
 }
